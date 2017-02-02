a16fa3ba4ea2387116df7d21a5fe774de876d0f1
Atnlie <atnlie@gmail.com>
2017-02-02 13:43:02 +0700

    search name from realmDB

---
+++ RealmDB/RealmModel/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  RealmDB
+//
+//  Created by Atnlie on 2/2/17.
+//  Copyright © 2017 Atnlie. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Person : Object{
+    dynamic var name = ""
+    dynamic var age = 0
+    dynamic var sex = ""
+}
